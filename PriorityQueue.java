e0accdd051670e20f1b620732b7e81823c90c928
EdwinMok <emok@stuy.edu>
2017-04-21 11:17:23 -0400

    edited interface to add comments

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,10 +1,15 @@
 public interface PriorityQueue<Ticket> {
 
+    //add a Ticket into the ArrayPriorityQueue
     public void add(Ticket x);
 
+    //check if the ArrayPQ is empty
     public boolean isEmpty();
 
+    //check the next Ticket to be processed based on priority
     public Ticket peekMin();
 
+    //remove the next Ticket after it has been processed
     public Ticket removeMin(); 
+
 }
